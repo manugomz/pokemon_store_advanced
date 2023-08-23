24537dbc9eeb48027f7d74511cc658bdcf9a394d
Manuela Gomez <manuelagomez1005@hotmail.com>
2023-08-22 23:09:34 -0500

    deleted card-details page and turning it into a modal window with URL change

--- scripts/buy-cards.js
+++ scripts/buy-cards.js
@@ -8,6 +8,7 @@ const arrowLeft = document.querySelector('.arrow-left');
 const heart = document.querySelectorAll('.heart');
 
 const $skeletonContainer = document.getElementById('cards-skeleton');
+const modal = document.getElementById('modal');
 
 
 /*-----------Global variables--------------*/
@@ -19,6 +20,7 @@ let type = 'all';
 let index = 0;
 let max = 20;
 let $template = "";
+let $templateModal = "";
 let $templateSkeleton = "";
 let likedPokemon = [];
 sessionStorage.likedPokemon = JSON.stringify();
@@ -154,13 +156,15 @@ const cardDisplay = async () => {
             <img class="pokemon-img" src="${pokemon[i].sprites.other['official-artwork'].front_default}" alt="${pokemon[i].name}" >
             <div class="card-text text-bottom">
                 <p><b>EXP: ${pokemon[i].base_experience}</b></p>
-                <a class='buy-link' href="./card-details.html?pokemon=${pokemon[i].id}"> <button class= "buy-button">Buy</button></a>
+                <a class='buy-link' href="./buy-cards.html?pokemon=${pokemon[i].id}"> <button class= "buy-button">Buy</button></a>
             </div>
         </div>
         `;
         $parentElement.innerHTML = $template;
         likedCards();
     }
+    const buyButtons = document.querySelectorAll('.buy-button');
+    return (buyButtons)
 }
 
 cardDisplay();
@@ -198,4 +202,153 @@ function likedCards() {
     )
 }
 
-//-----scroll to load cards-------
+//-------------------------Buy cards modal----------------------------------------
+
+//TODO show when click buy button
+//TODO! remove the initial bug (no card selected)
+
+window.addEventListener('locationchange', function () {
+    console.log('location changed!');
+});
+
+function clickBuyButton() {
+    modal.style.display = 'flex';
+}
+//From the other link
+const id = new URLSearchParams(window.location.search).get('pokemon');
+const endpoint = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+//$parentElement.textContent = endpoint;
+
+const singlePokemonData = async () => {
+    indivpokemon = await fetchApi(endpoint);
+    return indivpokemon;
+}
+
+const pokemonDisplay = async () => {
+    let pokemon = await singlePokemonData();
+    $templateModal = `
+        <p class="id"> #${pad(pokemon.id)} </p>
+            <img class="pokemon-img" src="${pokemon.sprites.other['official-artwork'].front_default}" alt="${pokemon.name}" >
+                <div class ="types">
+                    <p class ="type" ${typeColor(pokemon.types[0].type.name)}'> ${pokemon.types[0].type.name} </p>
+                    <p class ="type" ${typeColor(pokemon.types[1] && pokemon.types[1].type.name)}'> ${pokemon.types[1] && pokemon.types[1].type.name} </p>
+                </div>
+            <h1>${pokemon.name}</h1>
+            <div class= "wh">
+                <div class="height">
+                    <p class="stat-number"> ${pokemon.weight} </p>
+                    <p > Weight</p>
+                </div>
+                <div class="height">
+                    <p class="stat-number"> ${pokemon.height} </p>
+                    <p> Height </p>
+                </div>
+            </div>
+            <div class= "stats">
+                <p>HP</p>
+                <div class='bar'>
+                    <div class="data hp" style="width:${(pokemon.stats[0].base_stat / 255) * 100}%;"> 
+                    <p class='data-number'>${pokemon.stats[0].base_stat}/255</p>
+                    </div>
+                </div>
+                <p>ATK</p>
+                <div class='bar'>
+                    <div class="data atk" style="width:${(pokemon.stats[1].base_stat / 182) * 100}%;"> 
+                    <p class='data-number'>${pokemon.stats[1].base_stat}/182</p>
+                    </div>
+                </div>
+                <p>DEF</p>
+                <div class='bar'>
+                    <div class="data def" style="width:${(pokemon.stats[2].base_stat / 230) * 100}%;"> 
+                    <p class='data-number'>${pokemon.stats[2].base_stat}/230</p>
+                    </div>
+                </div>
+                <p>SPD</p>
+                <div class='bar'>
+                    <div class="data spd" style="width:${(pokemon.stats[5].base_stat / 200) * 100}%;"> 
+                    <p class='data-number'>${pokemon.stats[5].base_stat}/200</p>
+                    </div>
+                </div>
+                <p>EXP</p>
+                <div class='bar'>
+                    <div class="data exp" style="width:${(pokemon.base_experience / 635) * 100}%;"> 
+                    <p class='data-number'>${pokemon.base_experience}/635</p>
+                    </div>
+                </div>
+            </div>
+        `;
+    modal.innerHTML = $template;
+}
+
+pokemonDisplay();
+
+const typeColor = (type) => {
+    switch (type) {
+        case 'bug':
+            color = `style='background-color:#adbd21`;
+            break;
+        case 'dark':
+            color = `style='background-color:#765f51`;
+            break;
+        case 'dragon':
+            color = `style='background-color:#7761da`;
+            break;
+        case 'electric':
+            color = `style='background-color:#ffc631`;
+            break;
+        case 'fairy':
+            color = `style='background-color:#f7b5f7`;
+            break;
+        case 'fighting':
+            color = `style='background-color:#a55239`;
+            break;
+        case 'fire':
+            color = `style='background-color:#f75231`;
+            break;
+        case 'flying':
+            color = `style='background-color:#9cadf7`;
+            break;
+        case 'ghost':
+            color = `style='background-color:#6363b5`;
+            break;
+        case 'grass':
+            color = `style='background-color:#7bce52`;
+            break;
+        case 'ground':
+            color = `style='background-color:#d6b55a`;
+            break;
+        case 'ice':
+            color = `style='background-color:#5acee7`;
+            break;
+        case 'normal':
+            color = `style='background-color:#ada594`;
+            break;
+        case 'poison':
+            color = `style='background-color:#b55aa5`;
+            break;
+        case 'psychic':
+            color = `style='background-color:#ff73a5`;
+            break;
+        case 'rock':
+            color = `style='background-color:#bda55a`;
+            break;
+        case 'steel':
+            color = `style='background-color:#adadc6`;
+            break;
+        case 'water':
+            color = `style='background-color:#399cff`;
+            break;
+        case undefined:
+            color = `style= 'display:none'`;
+            break;
+    }
+    return color;
+}
+
+function pad(n) {
+    var len = 3 - ('' + n).length;
+    return (len > 0 ? new Array(++len).join('0') : '') + n
+}
+
+//TODO!-----scroll to load cards-------
